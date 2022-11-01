1cae775725021170ebe0ab01f1b2ae542586789e
ljy <link.twilight.gu@gmail.com>
2022-11-01 19:36:37 +0800

    自动配置

--- springbootLearn/stage01-bootstart/src/main/java/com/learn/controller/Startcontroller.java
+++ springbootLearn/stage01-bootstart/src/main/java/com/learn/controller/Startcontroller.java
@@ -1,24 +1,27 @@
 package com.learn.controller;
 
 import com.learn.properties.DataSourceProperties;
+import com.learn.properties.DataSourceProperties2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@EnableConfigurationProperties(DataSourceProperties2.class)
 public class Startcontroller {
     @RequestMapping("/hello")
     public String hello() {
         return "hello  Spring World";
     }
-
     @Resource
-    DataSourceProperties dataSourceProperties;
+//    DataSourceProperties dataSourceProperties;
+    DataSourceProperties2 dataSourceProperties2;
+
     @RequestMapping("/hello2")
-    public String dataTest(){
-    
-            return dataSourceProperties.toString();
-        }
+    public String dataTest() {
+        return dataSourceProperties2.toString();
+    }
 }
