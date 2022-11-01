dc567149c5b698cd6f6d7116e53cf089bdf29925
ljy <link.twilight.gu@gmail.com>
2022-11-01 18:19:36 +0800

    使用properties配置自定义的数据库属性, 如果打印成功, 说明配置正确

--- springbootLearn/stage01-bootstart/src/main/java/com/learn/controller/Startcontroller.java
+++ springbootLearn/stage01-bootstart/src/main/java/com/learn/controller/Startcontroller.java
@@ -1,13 +1,24 @@
 package com.learn.controller;
 
+import com.learn.properties.DataSourceProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class Startcontroller {
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
+        return "hello  Spring World";
+    }
 
-            return "hello  Spring World";
+    @Resource
+    DataSourceProperties dataSourceProperties;
+    @RequestMapping("/hello2")
+    public String dataTest(){
+    
+            return dataSourceProperties.toString();
         }
 }
