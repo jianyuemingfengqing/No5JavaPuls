5195676ccc8b405028bab464d8f2363e8b856592
ljy <link.twilight.gu@gmail.com>
2022-11-02 21:30:12 +0800

    原生 订阅发布 加消费者

---
+++ rabbitMQLearn/stage01-rabbitMQ/rabbitmq_consumer/src/main/java/com/learn/fanout/Consumer1.java
@@ -0,0 +1,28 @@
+package com.learn.fanout;
+
+import com.learn.utils.RabbitMQConnectionUtils;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer1 {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = RabbitMQConnectionUtils.getConnection();
+
+        Channel channel = connection.createChannel();
+
+        String queueName = "rabbitmq-fanout-queue1";
+
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                super.handleDelivery(consumerTag, envelope, properties, body);
+                System.out.println("consumer1: " + new String(body));
+            }
+        };
+
+        channel.basicConsume(queueName, true, consumer);
+
+    }
+}
