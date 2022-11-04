6e5d4f91695df721e8264962208fd5a73df584a8
ljy <link.twilight.gu@gmail.com>
2022-11-04 18:59:54 +0800

    确认模式 测试完成

--- rabbitMQLearn/stage03-rabbitMQpuls/rabbitmq-producer-spring/src/test/java/com/learn/test/ProducerTest.java
+++ rabbitMQLearn/stage03-rabbitMQpuls/rabbitmq-producer-spring/src/test/java/com/learn/test/ProducerTest.java
@@ -2,6 +2,7 @@ package com.learn.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -15,6 +16,18 @@ public class ProducerTest {
 
     @Test
     public void confirmTest(){
-    rabbitTemplate.convertAndSend("queue-confirm","confirm","test confirm  测试");
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                //ack  确认
+                if (ack){
+                    System.out.println("连接交换机成功");
+                } else {
+                    System.out.println("错误原因 " + cause);
+                }
+            }
+        });
+
+    rabbitTemplate.convertAndSend("confirm-exchange","confirm","test confirm  测试");
     }
 }
